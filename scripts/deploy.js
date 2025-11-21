4b64591a67a904ad1b7730802dc2c1a9aeda44bb
Legitimate-User <alpana.kumari1234@gmail.com>
2025-11-21 18:42:58 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,5 @@
 // scripts/deployStudentClubDAO.js
 
-/**
- * Hardhat deployment script for StudentClubDAO.sol
- * Usage: npx hardhat run scripts/deployStudentClubDAO.js --network <network-name>
- */
-
 const { ethers } = require("hardhat");
 
 async function main() {
@@ -72,3 +67,4 @@ main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
+
